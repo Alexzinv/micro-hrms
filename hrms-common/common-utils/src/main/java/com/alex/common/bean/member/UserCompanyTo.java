bad2fa69a9fa4260f83d8a2a47223fff511179eb
Alexzinv <1610425335@qq.com>
2021-12-06 16:04:50 +0800

    update: change the package

--- hrms-common/common-utils/src/main/java/com/alex/common/bean/member/UserCompanyTo.java
+++ hrms-common/common-utils/src/main/java/com/alex/common/bean/member/UserCompanyTo.java
@@ -1,4 +1,4 @@
-package com.alex.common.bean;
+package com.alex.common.bean.member;
 
 import com.baomidou.mybatisplus.annotation.*;
 import io.swagger.annotations.ApiModel;
--- hrms-common/common-utils/src/main/java/com/alex/common/bean/member/UserPersonalInfoTo.java
+++ hrms-common/common-utils/src/main/java/com/alex/common/bean/member/UserPersonalInfoTo.java
@@ -1,4 +1,4 @@
-package com.alex.common.bean;
+package com.alex.common.bean.member;
 
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
