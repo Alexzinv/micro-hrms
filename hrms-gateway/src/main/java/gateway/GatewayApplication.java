5ec558c5e843f87979ccc170a36504351f3b49d4
Alexzinv <1610425335@qq.com>
2021-12-07 10:12:14 +0800

    update: update gateway config

--- hrms-gateway/src/main/java/gateway/GatewayApplication.java
+++ hrms-gateway/src/main/java/gateway/GatewayApplication.java
@@ -1,11 +1,9 @@
 package gateway;
 
 import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
-import org.springframework.boot.Banner;
-import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
@@ -17,9 +15,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class GatewayApplication {
 
 	public static void main(String[] args) {
-		new SpringApplicationBuilder(GatewayApplication.class)
-				.bannerMode(Banner.Mode.OFF)
-				.web(WebApplicationType.SERVLET)
-				.run(args);
+		SpringApplication.run(GatewayApplication.class, args);
 	}
 }
