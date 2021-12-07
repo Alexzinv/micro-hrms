7ec069a198235c66b0bdc5ada2950df9e0486ffb
Alexzinv <1610425335@qq.com>
2021-12-07 15:02:36 +0800

    update: gateway filter

--- hrms-gateway/src/main/java/gateway/filter/AuthGlobalFilter.java
+++ hrms-gateway/src/main/java/gateway/filter/AuthGlobalFilter.java
@@ -1,6 +1,8 @@
 package gateway.filter;
 
+import com.alex.common.util.JwtUtils;
 import com.google.gson.JsonObject;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
@@ -9,6 +11,7 @@ import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.util.AntPathMatcher;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
@@ -20,30 +23,34 @@ import java.util.List;
  * @Description
  */
 @Component
+@Slf4j
 public class AuthGlobalFilter implements GlobalFilter, Ordered {
 
-	private AntPathMatcher antPathMatcher = new AntPathMatcher();
+	private final AntPathMatcher antPathMatcher = new AntPathMatcher();
+	private static final String AUTH = "/api/**/auth/**";
+	private static final String INNER = "/**/inner/**";
 
 	@Override
 	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
 		ServerHttpRequest request = exchange.getRequest();
 		String path = request.getURI().getPath();
-		//谷粒学院api接口，校验用户必须登录
-		if(antPathMatcher.match("/api/**/auth/**", path)) {
+		log.info("=================" + path);
+		//校验用户必须登录
+		if(antPathMatcher.match(AUTH, path)) {
 			List<String> tokenList = request.getHeaders().get("token");
-			if(null == tokenList) {
+			if(CollectionUtils.isEmpty(tokenList)) {
 				ServerHttpResponse response = exchange.getResponse();
 				return out(response);
 			} else {
-               // Boolean isCheck = JwtUtils.checkToken(tokenList.get(0));
-               // if(!isCheck) {
+               boolean isCheck = JwtUtils.checkToken(tokenList.get(0));
+               if(!isCheck) {
 				ServerHttpResponse response = exchange.getResponse();
 				return out(response);
-               // }
+               }
 			}
 		}
 		//内部服务接口，不允许外部访问
-		if(antPathMatcher.match("/**/inner/**", path)) {
+		if(antPathMatcher.match(INNER, path)) {
 			ServerHttpResponse response = exchange.getResponse();
 			return out(response);
 		}
@@ -62,8 +69,8 @@ public class AuthGlobalFilter implements GlobalFilter, Ordered {
 		message.addProperty("data", "鉴权失败");
 		byte[] bits = message.toString().getBytes(StandardCharsets.UTF_8);
 		DataBuffer buffer = response.bufferFactory().wrap(bits);
-		//response.setStatusCode(HttpStatus.UNAUTHORIZED);
-		//指定编码，否则在浏览器中会中文乱码
+		/// response.setStatusCode(HttpStatus.UNAUTHORIZED);
+		/// 指定编码，否则在浏览器中会中文乱码
 		response.getHeaders().add("Content-Type", "application/json;charset=UTF-8");
 		return response.writeWith(Mono.just(buffer));
 	}
