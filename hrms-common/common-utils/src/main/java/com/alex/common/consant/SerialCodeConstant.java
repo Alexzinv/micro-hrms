2fdf5da0f1077e3ce992260ab04b9a1a420f148b
Alexzinv <1610425335@qq.com>
2021-12-07 10:45:59 +0800

    add: add SerialCodeConstant

---
+++ hrms-common/common-utils/src/main/java/com/alex/common/consant/SerialCodeConstant.java
@@ -0,0 +1,19 @@
+package com.alex.common.consant;
+
+/**
+ * @author _Alexzinv_
+ * @date 2021/12/7
+ * @description
+ */
+public interface SerialCodeConstant {
+
+    /**
+     * 序列号 工号
+     */
+    int SERIAL = 10000;
+
+    /**
+     * 编码
+     */
+    int CODE = 1;
+}
