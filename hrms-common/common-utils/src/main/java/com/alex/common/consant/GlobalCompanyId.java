911598c11a191e3c52b08019b21119e9e2d7ce79
Alexzinv <1610425335@qq.com>
2021-12-10 13:58:23 +0800

    add: 添加全局保存companyId的全局变量

--- hrms-common/common-utils/src/main/java/com/alex/common/consant/GlobalCompanyId.java
+++ hrms-common/common-utils/src/main/java/com/alex/common/consant/GlobalCompanyId.java
@@ -3,8 +3,8 @@ package com.alex.common.consant;
 /**
  * @author _Alexzinv_
  * @date 2021/12/10
- * @description 获取当前用户关联的公司id,并暂时存储，提供给全局业务使用
- * 充当类似于一个抽象的常量的作用
+ * @description 在用户登录二阶段认证时，获取当前用户关联的公司id,并暂时存储，
+ *              提供给全局业务使用，充当类似于一个抽象的常量的作用
  */
 public class GlobalCompanyId {
 
