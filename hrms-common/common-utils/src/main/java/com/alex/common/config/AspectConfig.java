224c2754849a721f2ce4b2247e2ae3cb135b3d99
Alexzinv <1610425335@qq.com>
2021-12-21 14:06:33 +0800

    add: enable aspectj

---
+++ hrms-common/common-utils/src/main/java/com/alex/common/config/AspectConfig.java
@@ -0,0 +1,14 @@
+package com.alex.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+/**
+ * @author _Alexzinv_
+ * @date 2021/12/21
+ * @description
+ */
+@Configuration
+@EnableAspectJAutoProxy
+public class AspectConfig {
+}
