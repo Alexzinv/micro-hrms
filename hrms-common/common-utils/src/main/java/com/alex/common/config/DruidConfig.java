4c99e4a930cf7f74742a28314f485f6950258dce
Alexzinv <1610425335@qq.com>
2021-12-29 16:24:35 +0800

    update: add druid config

--- hrms-common/common-utils/src/main/java/com/alex/common/config/DruidConfig.java
+++ hrms-common/common-utils/src/main/java/com/alex/common/config/DruidConfig.java
@@ -1,21 +1,59 @@
 package com.alex.common.config;
 
 import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceBuilder;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author _Alexzinv_
  * @date 2021/12/27
  * @description
  */
+@Slf4j
 @Configuration
 public class DruidConfig {
 
     @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
     public DataSource druid(){
         return DruidDataSourceBuilder.create().build();
+    }
+
+    @Bean
+    public ServletRegistrationBean<StatViewServlet> druidServlet() {
+        log.info("init Druid Servlet Configuration ");
+        ServletRegistrationBean<StatViewServlet> servletRegistrationBean = new ServletRegistrationBean<>();
+        servletRegistrationBean.setServlet(new StatViewServlet());
+        servletRegistrationBean.addUrlMappings("/druid/*");
+        Map<String, String> initParameters = new HashMap<String, String>(16);
+        initParameters.put("loginUsername", "admin");
+        initParameters.put("loginPassword", "admin");
+        // 禁用HTML页面上的“Reset All”功能
+        initParameters.put("resetEnable", "false");
+        // IP白名单 (没有配置或者为空，则允许所有访问)
+        initParameters.put("allow", "");
+        // IP黑名单 (存在共同时，deny优先于allow)
+        /// initParameters.put("deny", "192.168.20.38");
+        servletRegistrationBean.setInitParameters(initParameters);
+        return servletRegistrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean<WebStatFilter> filterRegistrationBean() {
+        FilterRegistrationBean<WebStatFilter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new WebStatFilter());
+        filterRegistrationBean.addUrlPatterns("/*");
+        filterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
+        return filterRegistrationBean;
     }
 }
---
+++ hrms-common/common-utils/src/main/java/com/alex/common/filter/DruidStatFilter.java
@@ -0,0 +1,19 @@
+package com.alex.common.filter;
+
+import com.alibaba.druid.support.http.WebStatFilter;
+
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebInitParam;
+
+/**
+ * @author _Alexzinv_
+ * @date 2021/12/29
+ * @description 配置druid拦截规则
+ */
+@WebFilter(filterName = "druidWebStatFilter", urlPatterns = "/*",
+        initParams = {
+                // 忽略资源
+                @WebInitParam(name = "exclusions", value = "*.js,*.gif,*.jpg,*.bmp,*.png,*.css,*.ico,/druid/*")
+        })
+public class DruidStatFilter extends WebStatFilter {
+}
