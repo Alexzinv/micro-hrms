7abe252c49f67f27a9c4aa2107eb379d66a79070
Alexzinv <1610425335@qq.com>
2022-01-06 17:35:50 +0800

    update: 修改已存在查询，提高运行效率

--- hrms-company/src/main/java/com/alex/company/mapper/CompanyMapper.java
+++ hrms-company/src/main/java/com/alex/company/mapper/CompanyMapper.java
@@ -2,6 +2,7 @@ package com.alex.company.mapper;
 
 import com.alex.company.entity.Company;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Param;
 
 /**
  *
@@ -10,4 +11,10 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  */
 public interface CompanyMapper extends BaseMapper<Company> {
 
+    /**
+     * 判断公司名称是否存在
+     * @param name 公司名称
+     * @return bool
+     */
+    boolean isExist(@Param("name") String name);
 }
