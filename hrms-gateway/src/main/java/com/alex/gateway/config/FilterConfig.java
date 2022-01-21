492107a61b33a3552b7f80888c8caabc366e05d6
Alexzinv <1610425335@qq.com>
2022-01-21 11:27:10 +0800

    gateway:add:xss filter

---
+++ hrms-gateway/src/main/java/com/alex/gateway/config/FilterConfig.java
@@ -0,0 +1,28 @@
+package com.alex.gateway.config;
+
+import com.alex.gateway.xss.XssFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.DispatcherType;
+
+/**
+ * Filter配置
+ *
+ * @author _
+ */
+@Configuration
+public class FilterConfig {
+
+    @Bean
+    public FilterRegistrationBean<XssFilter> xssFilterRegistration() {
+        FilterRegistrationBean<XssFilter> registration = new FilterRegistrationBean<>();
+        registration.setDispatcherTypes(DispatcherType.REQUEST);
+        registration.setFilter(new XssFilter());
+        registration.addUrlPatterns("/*");
+        registration.setName("xssFilter");
+        registration.setOrder(Integer.MAX_VALUE);
+        return registration;
+    }
+}
