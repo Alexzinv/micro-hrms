b61d8daefbdbe7b867bc41bbe530dd4cc9605b75
Alexzinv <1610425335@qq.com>
2022-01-21 11:24:43 +0800

    common:add:util

---
+++ hrms-common/common-utils/src/main/java/com/alex/common/util/HttpContextUtils.java
@@ -0,0 +1,29 @@
+package com.alex.common.util;
+
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Objects;
+
+/**
+ * @author _Alexzinv_
+ * @date 2022/1/21
+ */
+public class HttpContextUtils {
+
+    public static HttpServletRequest getHttpServletRequest() {
+        return ((ServletRequestAttributes) Objects.requireNonNull(RequestContextHolder.getRequestAttributes())).getRequest();
+    }
+
+    public static String getDomain(){
+        HttpServletRequest request = getHttpServletRequest();
+        StringBuffer url = request.getRequestURL();
+        return url.delete(url.length() - request.getRequestURI().length(), url.length()).toString();
+    }
+
+    public static String getOrigin(){
+        HttpServletRequest request = getHttpServletRequest();
+        return request.getHeader("Origin");
+    }
+}
