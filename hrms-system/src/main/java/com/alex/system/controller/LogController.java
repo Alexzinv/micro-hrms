37e36136b9407647ce31bb84c0da085222d36af1
Alexzinv <1610425335@qq.com>
2022-01-22 20:05:26 +0800

    system:update:log

--- hrms-system/src/main/java/com/alex/system/controller/LogController.java
+++ hrms-system/src/main/java/com/alex/system/controller/LogController.java
@@ -1,13 +1,12 @@
 package com.alex.system.controller;
 
 import com.alex.common.util.R;
-import com.alex.common.valid.group.AddGroup;
+import com.alex.system.annotation.SysLog;
 import com.alex.system.dto.LogQuery;
 import com.alex.system.entity.Log;
 import com.alex.system.service.LogService;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,11 +22,11 @@ public class LogController {
     private final LogService logService;
 
     @Autowired
-    LogController(LogService logService){
+    public LogController(LogService logService){
         this.logService = logService;
     }
 
-    @GetMapping("/list/{page}/{limit}")
+    @PostMapping("/list/{page}/{limit}")
     public R listLogCondition(@PathVariable("page") Integer page,
                               @PathVariable("limit") Integer limit,
                               @RequestBody(required = false) LogQuery query){
@@ -35,6 +34,7 @@ public class LogController {
         return R.ok().data("records", result.getRecords()).data("total", result.getTotal());
     }
 
+    @SysLog("删除日志")
     @DeleteMapping("delete")
     public R removeLogBatch(List<Long> ids){
         logService.removeByIds(ids);
