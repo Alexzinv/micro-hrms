b6d9934857d0b2ec3a496128bb64a1fe1e6ef65b
Alexzinv <1610425335@qq.com>
2022-02-16 13:50:50 +0800

    common:add service constant

---
+++ hrms-common/common-utils/src/main/java/com/alex/common/consant/ServiceNameConstant.java
@@ -0,0 +1,36 @@
+package com.alex.common.consant;
+
+/**
+ * @author _Alexzinv_
+ * @date 2022/2/16
+ * @description 服务名枚举
+ */
+public interface ServiceNameConstant {
+
+    /** 网关    8888 */
+    String GATEWAY_SERVICE = "hrms-gateway";
+
+    /** 系统服务 9000 */
+    String SYSTEM_SERVICE = "hrms-system";
+
+    /** 三方服务 9100 */
+    String THIRD_PART_SERVICE = "third-part";
+
+    /** 公司服务 9200 */
+    String COMPANY_SERVICE = "hrms-company";
+
+    /** 成员服务 9300 */
+    String MEMBER_SERVICE = "hrms-member";
+
+    /** 绩效服务 9400 */
+    String PERFORMANCE_SERVICE = "hrms-performance";
+
+    /** 薪酬服务 9500 */
+    String SALARY_SERVICE = "hrms-salary";
+
+    /** 考勤服务 9600 */
+    String ATTENDANCE_SERVICE = "hrms-attendance";
+
+    /** 统计服务 10000 */
+    String STATISTIC_SERVICE = "hrms-statistic";
+}
